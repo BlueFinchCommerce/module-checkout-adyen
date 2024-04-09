51d759f4baf64c43b59eaf99f42b26767813521e
Liam Toohey <liam.toohey@gene.co.uk>
2024-04-09 15:19:58 +0100

    Port over Adyen logic

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Gene_[Name]',
+    'Gene_BetterCheckoutAdyen',
     __DIR__
 );
