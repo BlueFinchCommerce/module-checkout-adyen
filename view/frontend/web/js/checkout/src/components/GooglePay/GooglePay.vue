741b5c7e3eadd2f89ad5a061dcc8ac86e9e104a6
Alla Nehaenko <allanehaenko@gmail.com>
2025-01-14 16:26:35 +0000

    set loader to false

--- view/frontend/web/js/checkout/src/components/GooglePay/GooglePay.vue
+++ view/frontend/web/js/checkout/src/components/GooglePay/GooglePay.vue
@@ -354,6 +354,7 @@ export default {
           // is there and if not - cleat shipping address for checkout
           setTimeout(() => {
             if (!document.querySelector('gpay-graypane')) {
+              this.setLoadingState(false);
               clearCartAddresses();
             }
           }, 1000);
