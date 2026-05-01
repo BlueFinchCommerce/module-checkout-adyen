4d50cce9ef0561ef7b86c7c63d3b25cfa1ada757
Max Parker <max.parker@gene.co.uk>
2026-05-01 12:02:06 +0100

    Update to use Module\Manager to determine Adyen_ExpressCheckout enabled state

--- Plugin/CustomerData/AddAdyenExpressMethods.php
+++ Plugin/CustomerData/AddAdyenExpressMethods.php
@@ -6,43 +6,24 @@ namespace BlueFinch\CheckoutAdyen\Plugin\CustomerData;
 use Adyen\Payment\Api\AdyenPaymentMethodManagementInterface;
 use Magento\Checkout\CustomerData\Cart as Subject;
 use Magento\Checkout\Model\Session;
-use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\Module\Manager;
 
 class AddAdyenExpressMethods
 {
     public const PAYMENT_METHODS_KEY = 'bluefinch_adyen_payment_methods';
 
     /**
-     * @var AdyenPaymentMethodManagementInterface
-     */
-    private $adyenPaymentMethodManagement;
-
-    /**
-     * @var Session
-     */
-    private $checkoutSession;
-
-    /**
-     * @var ScopeConfigInterface
-     */
-    private $scopeConfig;
-
-    /**
      * Cart constructor
      *
      * @param AdyenPaymentMethodManagementInterface $adyenPaymentMethodManagement
      * @param Session $checkoutSession
-     * @param ScopeConfigInterface $scopeConfig
+     * @param Manager $moduleManager
      */
     public function __construct(
-        AdyenPaymentMethodManagementInterface $adyenPaymentMethodManagement,
-        Session $checkoutSession,
-        ScopeConfigInterface $scopeConfig
+        private readonly AdyenPaymentMethodManagementInterface $adyenPaymentMethodManagement,
+        private readonly Session $checkoutSession,
+        private readonly Manager $moduleManager
     ) {
-        $this->adyenPaymentMethodManagement = $adyenPaymentMethodManagement;
-        $this->checkoutSession = $checkoutSession;
-        $this->scopeConfig = $scopeConfig;
     }
 
     /**
@@ -56,13 +37,10 @@ class AddAdyenExpressMethods
         Subject $subject,
         array $result
     ): array {
-        $googlePayExpressEnabled = $this->scopeConfig->getValue('payment/adyen_googlepay/express_show_on', ScopeInterface::SCOPE_STORE);
-        $applePayExpressEnabled = $this->scopeConfig->getValue('payment/adyen_applepay/express_show_on', ScopeInterface::SCOPE_STORE);
-        $payPalExpressEnabled = $this->scopeConfig->getValue('payment/adyen_paypal_express/express_show_on', ScopeInterface::SCOPE_STORE);
 
         // Only run if no Adyen Express methods are enabled otherwise we'd be duplicating the Adyen API request.
-        if (!$googlePayExpressEnabled && !$applePayExpressEnabled && !$payPalExpressEnabled) {
-            $quoteId = $this->checkoutSession->getQuoteId();
+        if (!$this->moduleManager->isEnabled('Adyen_ExpressCheckout')) {
+            $quoteId = (string) $this->checkoutSession->getQuoteId();
             $paymentMethods = $quoteId ?
                 json_decode(
                     $this->adyenPaymentMethodManagement->getPaymentMethods($quoteId),
